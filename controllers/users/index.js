67dca844b055a1ceb251b6bda6d52b71479813de
AnastasiiaKotielnikova <akotelnikova1993@gmail.com>
2023-02-03 11:25:10 +0200

    add nodemailer

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -3,5 +3,15 @@ const login = require("./login");
 const current = require("./current");
 const logout = require("./logout");
 const avatar = require("./avatar");
+const verifyEmail = require("./verifyEmail");
+const resendEmail = require("./resendEmail");
 
-module.exports = { register, current, login, logout, avatar };
+module.exports = {
+  register,
+  current,
+  login,
+  logout,
+  avatar,
+  verifyEmail,
+  resendEmail,
+};
